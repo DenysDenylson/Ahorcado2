48b7524f4cb92ad24897a2c6bf9364ad309bebe4
DenysDenylson <DenysDenylson@gmail.com>
2014-11-06 05:59:42 -0400

    juego terminado al 95%

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
 require "sinatra"
 
 
+def esta?(palabra, letra)
+	if palabra =~ /letra/
+		return true
+	else
+		return false
+	end
+end
+
+def aparecer_letra(letra, palabra, palabra_secreta)
+
+	for i in 0...palabra_secreta.length
+		if palabra_secreta[i] == letra
+			palabra[i] = letra
+		end
+	end
+
+	return palabra
+end
 
 get '/' do
   erb :bienvenida
@@ -8,9 +26,27 @@ end
 
 post '/jugar' do
 	@palabra_secreta = params[:palabra]
+	@palabra_jugar = Array.new(@palabra_secreta.size) { '-' }
+
+	@palabra_jugar = @palabra_jugar.join('')
+
+	
+	
+
+	
+	@intentos = 0
 	erb :jugar
 end
 
 post '/validar_letra' do
-  @letra = params[:letra]
+	@letra = params[:letra]
+	@intentos = params[:intentos].to_i
+  	@palabra_secreta = params[:palabra]
+  	@palabra_jugar = params[:palabra_jugar]
+
+  	if @intentos == 3
+  		"PERDISTE la palabra secreta es::: #{@palabra_secreta}"
+  	else
+		erb :jugar
+  	end
 end
